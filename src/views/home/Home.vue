64510a42d63afaf7f6a7a205f9a1447ff484909b
guozhen <guozhen4213@126.com>
2020-08-02 18:54:06 +0800

    代码优化

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <home-header></home-header>
-    <home-swiper :list="data.swiperList"></home-swiper>
-    <home-icons :list="data.iconList"></home-icons>
-    <home-recommend :list="data.recommendList"></home-recommend>
-    <home-weekend :list="data.weekendList"></home-weekend>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
   </div>
 </template>
 
@@ -17,7 +17,7 @@ import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
 import { useStore } from 'vuex'
-import { reactive, computed, onMounted, onActivated } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
 export default {
   name: 'Home',
   components: {
@@ -28,42 +28,30 @@ export default {
     HomeWeekend
   },
   setup() {
-    const data = reactive({
-      lastCity: '',
-      swiperList: [],
-      iconList: [],
-      recommendList: [],
-      weekendList: []
-    })
+    // 生成响应式data
+    const swiperList = ref([])
+    const iconList = ref([])
+    const recommendList = ref([])
+    const weekendList = ref([])
+
     const store = useStore()
-    const city = computed(() => {
-      return store.state.city
-    })
-    function getHomeInfoSucc (res) {
+    const city = store.state.city
+    async function getHomeInfo () {
+      let res = await axios.get('/api/index.json?city=' + city)
       res = res.data
       console.log(res)
       if (res.ret && res.data) {
         const result = res.data
-        data.swiperList = result.swiperList
-        data.iconList = result.iconList
-        data.recommendList = result.recommendList
-        data.weekendList = result.weekendList
+        swiperList.value = result.swiperList
+        iconList.value = result.iconList
+        recommendList.value = result.recommendList
+        weekendList.value = result.weekendList
       }
     }
-    function getHomeInfo () {
-      axios.get('/api/index.json?city=' + city.value).then(getHomeInfoSucc)
-    }
     onMounted(() => {
-      data.lastCity = city
       getHomeInfo()     
     })
-    onActivated(() => {
-      if (data.lastCity !== city) {
-        data.lastCity = city
-        getHomeInfo()
-      }      
-    })
-    return { data }
+    return { swiperList, iconList, recommendList, weekendList }
   }
 }
 </script>
